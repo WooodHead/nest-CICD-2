8fdadf25eb8bff757e19f2c23cde16162f49dbac
livemehere <rhdxoals@gmail.com>
2022-07-03 23:56:05 +0900

    update 정상 임계값

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Github Actions CI/CD!! Second!!!!';
+    return 'Hello Github Actions CI/CD!! Third!!!!';
   }
 }
